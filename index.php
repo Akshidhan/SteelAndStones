52f9bba4139b1c45830c105e660a4e4fac6906ae
Akshidhan <wakshiwije@gmail.com>
2024-11-20 14:33:58 +0530

    Done user account

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
     <!-- navbar -->
     <nav class="navbar navbar-expand-lg navcolor">
         <div class="container-fluid mx-3">
-          <a class="navbar-brand" href="index.html"><img src="files/logo-no-background 1.png" alt="logo" style="width: 90.36px; height: 46px;"></a>
+          <a class="navbar-brand" href="index.php"><img src="files/logo-no-background 1.png" alt="logo" style="width: 90.36px; height: 46px;"></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
